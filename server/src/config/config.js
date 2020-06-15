b6fc9cd195fcbdeebd804ee4681154c49d0926ed
Ruma Das <rumaadl16@gmail.com>
2020-06-15 10:10:49 +0930

    adding models in projects

---
+++ server/src/config/config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    port: process.env.PORT || 8081,
+    db: {
+       database: process.env.DB_NAME || 'musictracker',
+       user:  process.env.DB_USER || 'musictracker',
+       password:  process.env.DB_PASSWORD || 'musictracker',
+       options: {
+           dialect:  process.env.DB_DIALECT || 'sqlite',
+           host:  process.env.DB_HOST || 'localhost',
+           storage: './musictracker.squlite'
+
+       }
+    }
+}
---
+++ server/src/models/User.js
@@ -0,0 +1,10 @@
+module.exports = (sequelize, dataType) => 
+    sequelize.define('User', {
+        email: {
+            type: dataType.STRING,
+            unique: true
+        },
+        password: dataType.STRING
+
+    })
+
