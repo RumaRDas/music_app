e829694b18cad048aa21f06acbcfc6d31575ce7a
Ruma Das <rumaadl16@gmail.com>
2020-06-16 11:55:27 +0930

    adding button link header

--- client/src/components/Header.vue
+++ client/src/components/Header.vue
@@ -1,15 +1,31 @@
 <template>
-  <v-card
-    color="grey lighten-4" flat height="50px" tile>
+  <v-card color="grey lighten-4" flat height="50px" tile>
     <v-toolbar dense>
-
-      <v-toolbar-title>Musuc App</v-toolbar-title>
+      <v-toolbar-title>
+        <span @click="navigateTo({name: 'root'})">Musuc App</span>
+      </v-toolbar-title>
+      <!-- <v-spacer></v-spacer>
+    <v-toolbar-items>
+      <v-btn flat>
+        Browse
+      </v-btn>
+      </v-toolbar-items>-->
+      <v-spacer></v-spacer>
+      <v-toolbar-items>
+        <v-btn flat @click="navigateTo({name: 'register'})">Sign Up</v-btn>
+      </v-toolbar-items>
     </v-toolbar>
   </v-card>
 </template>
 
 <script>
-export default {}
+export default {
+  methods: {
+    navigateTo(route) {
+      this.$router.push(route);
+    }
+  }
+};
 </script>
 
 <style scoped>
